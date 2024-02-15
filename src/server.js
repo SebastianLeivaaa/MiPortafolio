99170e8952214ae1ea45f6f62b6c68777c492dc2
SebastianLeivaaa <sebastianleiva010@gmail.com>
2024-02-15 16:52:11 -0300

    Configurando servidor

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
 //create express server
 import express from "express";
 import sendTestEmail  from "./utils/mailer.js";
-import cors from 'cors';
 
 
 const app = express();
-app.use(express.json());
-const port = 3001;
-app.use(cors());
+
 const SENDER_EMAIL_ID = "sebastianleiva010@gmail.com";
 
 app.post("/send-email", async (req, res) => {
@@ -25,8 +22,4 @@ app.post("/send-email", async (req, res) => {
   } catch (error) {
     res.send(error);
   }
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
 });
