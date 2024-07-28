1f1510664152c43ccbfd94aaeb2ee4e83ab5ed70
SebastianLeivaa <sebastianleiva26@gmail.com>
2024-07-28 17:41:37 -0400

    Actualizando refresh token

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 
 const CLIENT_ID = "753573338206-m8fprndcmdokn0j4j69evqbl0ie88jng.apps.googleusercontent.com";
 const CLIENT_SECRET = "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";
-const REFRESH_TOKEN = "1//04eYMGr982EILCgYIARAAGAQSNwF-L9IrwI3AMZFgfG_d4hibpd4hjPIHIQuB9fJWdiwEweuwH-KumVXWtQ5xWPZaoFbWzPmFTTA";
+const REFRESH_TOKEN = "1//04a0buqfa-vHOCgYIARAAGAQSNwF-L9IrZukN3KhVeGW1NQsTynzpiloGOE07PPRsHz2an4Z-M1GfoUffHvpVmb9xpomzkB4iG3I";
 const REDIRECT_URI = "https://developers.google.com/oauthplayground"; //DONT EDIT THIS
 const MY_EMAIL = "sebastianleiva010@gmail.com";
 
