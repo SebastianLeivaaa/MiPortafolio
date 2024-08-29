7ccdb15e56c69243fd833175fc7ed51861c8bf78
SebaLeivaa <sebastiannicolas.leiva@alumnos.ulagos.cl>
2024-08-29 15:03:03 -0400

    Actualizando refresh token

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 
 const CLIENT_ID = "753573338206-m8fprndcmdokn0j4j69evqbl0ie88jng.apps.googleusercontent.com";
 const CLIENT_SECRET = "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";
-const REFRESH_TOKEN = "1//04ez36wZaqWUTCgYIARAAGAQSNwF-L9Irapra4Gm4vpqy14EKhUh26hlxWLeiNfUeRhFH9qSdI4fbNdvd2HRBa74OMejo8ulkWAE";
+const REFRESH_TOKEN = "1//04Fp83fiQUpHSCgYIARAAGAQSNwF-L9Irlesqb_-NpQZsBfUQ03ROwSB5fW6yYjBd8DFGST6_eYvlzh6rtzAVB4gLi-KwT7xjoVI";
 const REDIRECT_URI = "https://developers.google.com/oauthplayground"; //DONT EDIT THIS
 const MY_EMAIL = "sebastianleiva010@gmail.com";
 
